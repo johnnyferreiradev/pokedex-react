fc4ebf41558c8ad9e136b1cc96add59284b538cf
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2022-04-02 16:40:04 -0300

    Removing unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-
-import TestComponent from 'components/TestComponent';
 
 import './App.css';
 
 function App() {
-  const a = 1;
-  const b = 2;
-
-  console.log(a, b);
-
-  return (
-    <div className="App">
-      <TestComponent />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">App</div>;
 }
 
 export default App;
